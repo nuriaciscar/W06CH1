639bc766fa29b7f23afa5bf9929dfe6c3d6d4397
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-01 15:53:27 +0100

    Created test Fom component

---
+++ src/components/Form.test.js
@@ -0,0 +1,11 @@
+import Form from "./Form";
+import React from "react";
+import ReactTestRender from "react-test-renderer";
+import { BrowserRouter as Router } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+
+describe("Given a Form component", () => {
+  describe("When it receives", () => {
+    test("Then", () => {});
+  });
+});
