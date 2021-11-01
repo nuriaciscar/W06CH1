fa6ff4a9abe9ad5be1fe0fcfda694f3776b9b161
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-01 17:10:32 +0100

    Created Task component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import Homepage from "./pages/Homepage";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <h1>To Do</h1>
+      <Form />
     </div>
   );
 }
---
+++ src/components/Task/Task.js
@@ -0,0 +1,5 @@
+const Task = (text) => {
+  return <p>{text}</p>;
+};
+
+export default Task;
