96f5021ba1531841452746ebddbd2400ebed23aa
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-11-01 18:03:00 +0100

    Added return with ul element on Tasks

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,19 +1,23 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import useToDoList from "../../hooks/useToDoList";
+import Task from "../Task/Task";
 
 const Tasks = () => {
-  const {toDoList, loadToDoList} = useToDoList();
-  const dispatch = useDispatch ();
- 
+  const { toDoList, loadToDoList } = useToDoList();
+  const dispatch = useDispatch();
+
   useEffect(() => {
     loadToDoList();
-  }, [dispatch]);
+  }, [dispatch, loadToDoList]);
 
-  return(
-  toDoList,
-  loadToDoList
+  return (
+    <ul>
+      {toDoList.map((toDo) => {
+        <Task key={toDo.id} name={toDo.name} />;
+      })}
+    </ul>
   );
-}
+};
 
 export default Tasks;
